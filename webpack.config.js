45a30dfd3ee55abb5c5c7024afaf3a8f016b8528
Papazon <1650mm@gmail.com>
2024-11-29 18:29:55 +0800

    Optimize webpack bundle splitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'btc-simulator.[contenthash].js',
+    filename: '[name].[contenthash].js',
     library: 'BTCSimulator',
     libraryTarget: 'umd',
     globalObject: 'this',
@@ -29,37 +30,73 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [
-                  'tailwindcss',
-                  'autoprefixer',
-                ],
-              },
-            },
-          },
-        ],
+          'postcss-loader'
+        ]
       }
     ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({
+      terserOptions: {
+        compress: {
+          drop_console: true,
+        },
+      },
+    })],
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: 5,
+      minSize: 20000,
+      cacheGroups: {
+        react: {
+          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+          name: 'react',
+          chunks: 'all',
+          priority: 40
+        },
+        recharts: {
+          test: /[\\/]node_modules[\\/](recharts|d3-*)[\\/]/,
+          name: 'recharts',
+          chunks: 'all',
+          priority: 30
+        },
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+          priority: 20
+        }
+      }
+    }
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
-      inject: true
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      }
     }),
     new MiniCssExtractPlugin({
       filename: 'styles.[contenthash].css'
     })
   ],
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-      name: 'vendor'
-    }
-  },
   resolve: {
     extensions: ['.js', '.jsx']
+  },
+  performance: {
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+    hints: false
   }
-};
+}
